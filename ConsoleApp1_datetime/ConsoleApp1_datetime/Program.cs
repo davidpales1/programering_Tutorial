3f344c0e4344f134dc79b2b2942209ee4f0f4e52
Jihad El Sayed <david.pales1@gmail.com>
2022-01-13 13:32:03 +0100

    sdf

---
+++ ConsoleApp1_datetime/ConsoleApp1_datetime/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1_datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime dt1 = new DateTime();
+            dt1 = DateTime.Now;
+            DateTime dt2 = new DateTime();
+
+            TimeSpan diff = dt2 - dt1;
+            dt2 = dt2.AddDays(5);
+            TimeSpan diff2 = dt2 - dt2;
+            TimeSpan diff3 = dt2.Subtract(dt2);
+            DateTime dt3 = new DateTime(2022, 01, 11);
+            DateTime dt4 = new DateTime();
+            dt4 = Convert.ToDateTime("2020-01-11");
+            bool isValidDate = DateTime.TryParse("2020-01-11", out DateTime dt5);
+            bool isParseExact = DateTime.TryParseExact("2020-01-11", "yyyy-MM-dd",  CultureInfo.CurrentCulture
+                , DateTimeStyles.None ,out DateTime dt6);
+        }
+
+        public string Format(string fmt, object arg, IFormatProvider formatProvider)
+        {
+            // Provide default formatting if arg is not an Int64.
+            if (arg.GetType() != typeof(Int64))
+                try
+                {
+                    return HandleOtherFormats(fmt, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", fmt), e);
+                }
+
+            // Provide default formatting for unsupported format strings.
+            string ufmt = fmt.ToUpper(CultureInfo.InvariantCulture);
+            if (!(ufmt == "H" || ufmt == "I"))
+                try
+                {
+                    return HandleOtherFormats(fmt, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", fmt), e);
+                }
+
+            // Convert argument to a string.
+            string result = arg.ToString();
+
+            // If account number is less than 12 characters, pad with leading zeroes.
+            if (result.Length < ACCT_LENGTH)
+                result = result.PadLeft(ACCT_LENGTH, '0');
+            // If account number is more than 12 characters, truncate to 12 characters.
+            if (result.Length > ACCT_LENGTH)
+                result = result.Substring(0, ACCT_LENGTH);
+
+            if (ufmt == "I")                    // Integer-only format.
+                return result;
+            // Add hyphens for H format specifier.
+            else                                         // Hyphenated format.
+                return result.Substring(0, 5) + "-" + result.Substring(5, 3) + "-" + result.Substring(8);
+        }
+    }
+}
---
+++ ConsoleApp2_inheritance/ConsoleApp2_inheritance/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Vehicle car1 = new Vehicle("volvo", "23 fd");
+            Car car2 = new Car("firare", "23 fd", 5);
+            Vehicle car3= new Car("tesla", "23 fd", 5);
+            Vehicle MotorCycle = new MotorCycle("motor volvi", "423 ffdghd");
+
+            //Vehicle car4= new Vehicle();
+            List<Vehicle> vehicles =new List<Vehicle>();
+            vehicles.Add(car1);
+            vehicles.Add(car2);
+            vehicles.Add(car3);
+            vehicles.Add(MotorCycle);
+            List<Vehicle> sortedVehicles = vehicles.OrderBy(vehicles=> vehicles.GetType()).ThenBy(vehicles => vehicles.Brand).ToList();
+            Console.WriteLine("Brand " + "Model " + "Extra Info " + "Vehicle Type");
+            Console.WriteLine("==================================================");
+
+            foreach (Vehicle vehicle in sortedVehicles)
+            {
+                Console.WriteLine(vehicle.Brand + " " + vehicle.Model + " " + vehicle.GetExtraInfo() + " " + vehicle.GetType());
+
+                //if(Vehicle is Car)
+                //   {
+                //     Console.WriteLine(vehicle.Brand +""+ (Vehicle as Car).NumbersOfDors);
+                // }
+            }
+            Console.ReadLine();
+        }
+        class Vehicle
+        {
+            public Vehicle(string brand, string model)
+            {
+                this.Brand = brand;
+                this.Model = model;
+            }
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public virtual string GetExtraInfo()
+            {
+                return "";
+            }
+            public virtual string GetType()
+            {
+                return "Vehicle";
+            }
+        }
+        class Car : Vehicle
+        {
+            public Car(string brand, string model, int numbersOfDors) : base(brand, model)
+            {
+                NumbersOfDors = numbersOfDors;
+            }
+            public int NumbersOfDors { get; set; }
+            public override string GetExtraInfo()
+            {
+                return "" + NumbersOfDors;
+            }
+            public override string GetType()
+            {
+                return "Car";
+            }
+
+        }
+        class MotorCycle : Vehicle
+        {
+            public MotorCycle(string brand, string model) : base(brand, model)
+            {
+
+            }
+            public override string GetType()
+            {
+                return "MotorCycle";
+            }
+        }
+
+    }
+}
