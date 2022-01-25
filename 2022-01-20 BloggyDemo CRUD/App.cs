9454517746a656cae2334cab14e0405b7bb713ca
Jihad El Sayed <david.pales1@gmail.com>
2022-01-25 13:04:19 +0100

    nb

---
+++ 2022-01-20 BloggyDemo CRUD/App.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Bloggy.Demo.Domain;
+
+namespace Bloggy.Demo
+{
+    public class App
+    {
+        DataAccess _dataAccess = new DataAccess();
+
+        public void Run()
+        {
+            PageMainMenu();
+        }
+
+        private void PageMainMenu()
+        {
+            Header("Huvudmeny");
+
+            ShowAllBlogPostsBrief();
+
+            WriteLine("Vad vill du göra?");
+            WriteLine("a) Gå till huvudmenyn");
+            WriteLine("b) Skapa en bloggpost");
+            WriteLine("c) Uppdatera en bloggpost");
+            WriteLine("d) Ta bort en bloggpost");
+
+            ConsoleKey command = Console.ReadKey(true).Key;
+
+            if (command == ConsoleKey.A)
+                PageMainMenu();
+
+            if (command == ConsoleKey.B)
+                PageCreatePost();
+
+            if (command == ConsoleKey.C)
+                PageUpdatePost();
+
+            if (command == ConsoleKey.D)
+                PageDeletePost();
+
+            PageMainMenu();
+        }
+
+        private void PageDeletePost()
+        {
+            Header("Ta bort");
+
+            ShowAllBlogPostsBrief();
+
+            Write("Vilken bloggpost vill du ta bort? ");
+
+            int postId = int.Parse(Console.ReadLine());
+
+            BlogPost blogpost = _dataAccess.GetPostById(postId);
+
+            _dataAccess.DeleteBlogpost(blogpost);
+
+            Write("Bloggposten är borttagen.");
+            Console.ReadKey();
+            PageMainMenu();
+        }
+
+        private void PageCreatePost()
+        {
+            Header("Skapa");
+
+            Write("Skriv in en titel: ");
+            string newTitle = Console.ReadLine();
+            Write("Skriv in en författare: ");
+            string newAuthor = Console.ReadLine();
+
+            BlogPost newBlogPost = new BlogPost();
+            newBlogPost.Author = newAuthor;
+            newBlogPost.Title = newTitle;
+
+            _dataAccess.CreateBlogpost(newBlogPost);
+
+            Write("Bloggposten är skapad.");
+            Console.ReadKey();
+            PageMainMenu();
+        }
+
+        private void PageUpdatePost()
+        {
+            Header("Uppdatera");
+
+            ShowAllBlogPostsBrief();
+
+            Write("Vilken bloggpost vill du uppdatera? ");
+
+            int postId = int.Parse(Console.ReadLine());
+
+            BlogPost blogpost = _dataAccess.GetPostById(postId);
+
+            WriteLine("Den nuvarande titeln är: " + blogpost.Title);
+
+            Write("Skriv in ny titel: ");
+
+            string newTitle = Console.ReadLine();
+
+            blogpost.Title = newTitle;
+
+            _dataAccess.UpdateBlogpost(blogpost);
+
+            Write("Bloggposten uppdaterad.");
+            Console.ReadKey();
+            PageMainMenu();
+        }
+
+        private void ShowAllBlogPostsBrief()
+        {
+            List<BlogPost> list = _dataAccess.GetAllBlogPostsBrief();
+
+            foreach (BlogPost bp in list)
+            {
+                WriteLine(bp.Id.ToString().PadRight(5) + bp.Title.PadRight(30) + bp.Author.PadRight(20));
+            }
+            WriteLine();
+        }
+
+        private void Header(string text)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine();
+        }
+
+        private void WriteLine(string text = "")
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(text);
+        }
+
+        private void Write(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(text);
+        }
+    }
+}
---
+++ 2022-01-20 BloggyDemo CRUD/DataAccess.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Bloggy.Demo.Domain;
+
+namespace Bloggy.Demo
+{
+    public class DataAccess
+    {
+        private const string conString = "Server=(localdb)\\mssqllocaldb; Database=BlogPostDemo";
+
+        public List<BlogPost> GetAllBlogPostsBrief()
+        {
+            var sql = @"SELECT [Id], [Author], [Title]
+                        FROM BlogPost";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                var list = new List<BlogPost>();
+
+                while (reader.Read())
+                {
+                    var bp = new BlogPost
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Author = reader.GetSqlString(1).Value,
+                        Title = reader.GetSqlString(2).Value
+                    };
+                    list.Add(bp);
+                }
+
+                return list;
+
+            }
+        }
+
+        public BlogPost GetPostById(int postId)
+        {
+            var sql = @"SELECT [Id], [Author], [Title]
+                        FROM BlogPost 
+                        WHERE Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", postId));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var bp = new BlogPost
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Author = reader.GetSqlString(1).Value,
+                        Title = reader.GetSqlString(2).Value
+                    };
+                    return bp;
+
+                }
+
+                return null;
+
+            }
+        }
+
+        public void UpdateBlogpost(BlogPost blogPost)
+        {
+            var sql = "UPDATE BlogPost SET Title=@Title WHERE id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", blogPost.Id));
+                command.Parameters.Add(new SqlParameter("Title", blogPost.Title));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteBlogpost(BlogPost blogPost)
+        {
+            var sql = "DELETE FROM BlogPost WHERE id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", blogPost.Id));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void CreateBlogpost(BlogPost blogPost)
+        {
+            var sql = "INSERT INTO BlogPost(Title, Author) VALUES(@title,@author)";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("@author", blogPost.Author));
+                command.Parameters.Add(new SqlParameter("@title", blogPost.Title));
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
